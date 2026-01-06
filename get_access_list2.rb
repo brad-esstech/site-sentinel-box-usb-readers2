ee996c14e837feda2551ec3fcd126e456de3ab45
brad-esstech <bbarton@esstech.com.au>
2026-01-06 22:53:25 +0800

    Update and rename get_access_list.rb to get_access_list2.rb

--- get_access_list2.rb
+++ get_access_list2.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
-require_relative 'config.rb' # load site config
+require_relative 'config2.rb' # load site config
 require 'httparty'
 
 #api_url = "https://staging.sitesentinel.com/api/gates/access_tokens"
@@ -11,7 +11,7 @@ response = HTTParty.get(api_url, {
 })
 
 if response.code == 200
-  File.write("/home/ubuntu/site-sentinel-box-usb-readers/access_list.txt", response.body)
+  File.write("/home/ubuntu/site-sentinel-box-usb-readers2/access_list2.txt", response.body)
 else
   puts "ERROR!"
 end
